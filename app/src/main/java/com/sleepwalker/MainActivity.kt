2aff3f7e0e2ec222288acfff1cac4d1021e2dcb4
zNitche <53448802+zNitche@users.noreply.github.com>
2023-08-17 17:41:23 +0200

    added permissions request dialog

--- app/src/main/java/com/sleepwalker/MainActivity.kt
+++ app/src/main/java/com/sleepwalker/MainActivity.kt
@@ -1,26 +1,65 @@
 package com.sleepwalker
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.sleepwalker.presentation.models.HeartBeatViewModel
-import com.sleepwalker.presentation.models.HeartBeatViewModelFactory
-import com.sleepwalker.presentation.views.SleepwalkerApp
+import com.sleepwalker.presentation.models.MainViewModel
+import com.sleepwalker.presentation.models.MainViewModelFactory
+import com.sleepwalker.presentation.views.MainView
 
+
+const val PERMISSIONS_REQUEST_CODE = 100
+val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.BODY_SENSORS)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        if (!hasPermissions()) {
+            requestPermissions(REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE)
+        }
+
         val healthService = (application as MainApplication).healthService
 
         setContent {
-            val healthViewModel = viewModel<HeartBeatViewModel>(
-                factory = HeartBeatViewModelFactory(healthService=healthService)
+            val mainViewModel = viewModel<MainViewModel>(
+                factory = MainViewModelFactory(healthService=healthService)
             )
 
-            SleepwalkerApp(healthViewModel = healthViewModel)
+            MainView(viewModel = mainViewModel)
+        }
+    }
+
+    private fun hasPermissions(): Boolean {
+        var state = true
+
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                state = false
+                break
+            }
+        }
+
+        return state
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE -> {
+                if ((grantResults.isEmpty() ||
+                            grantResults[0] != PackageManager.PERMISSION_GRANTED)) {
+                    finishAndRemoveTask()
+                }
+                return
+            }
+
+            else -> {}
         }
     }
 }
