7e957ed2536fb7749d58b04151489f3dd1db38de
zNitche <53448802+zNitche@users.noreply.github.com>
2023-08-20 17:55:54 +0200

    added Scaffold to SettingsView

--- app/src/main/java/com/sleepwalker/presentation/views/SettingsView.kt
+++ app/src/main/java/com/sleepwalker/presentation/views/SettingsView.kt
@@ -4,7 +4,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.wear.compose.material.AutoCenteringParams
+import androidx.wear.compose.material.PositionIndicator
+import androidx.wear.compose.material.Scaffold
 import androidx.wear.compose.material.ScalingLazyColumn
+import androidx.wear.compose.material.TimeText
+import androidx.wear.compose.material.Vignette
+import androidx.wear.compose.material.VignettePosition
 import androidx.wear.compose.material.rememberScalingLazyListState
 import com.sleepwalker.APP_TAG
 import com.sleepwalker.presentation.models.SettingsViewModel
@@ -13,13 +18,27 @@ import com.sleepwalker.presentation.models.SettingsViewModel
 fun SettingsView(viewModel: SettingsViewModel) {
     val listState = rememberScalingLazyListState()
 
-    ScalingLazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        autoCentering = AutoCenteringParams(itemIndex = 0),
-        state = listState
+    Scaffold(
+        timeText = {
+            TimeText()
+        },
+        vignette = {
+            Vignette(vignettePosition = VignettePosition.TopAndBottom)
+        },
+        positionIndicator = {
+            PositionIndicator(
+                scalingLazyListState = listState
+            )
+        }
     ) {
+        ScalingLazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            autoCentering = AutoCenteringParams(itemIndex = 0),
+            state = listState
+        ) {
 
-        item { AppTitleText("$APP_TAG - Settings") }
-        item { GoBackNavButton(viewModel.navController) }
+            item { AppTitleText("$APP_TAG - Settings") }
+            item { GoBackNavButton(viewModel.navController) }
+        }
     }
 }
